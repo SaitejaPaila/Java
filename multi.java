e9be91db95937cac1c45e0e1e99b23450a7c91c6
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2021-09-08 20:51:53 -0500

    Add files via upload

---
+++ multi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class multi {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the value:");
+		int m = in.nextInt();
+		System.out.print("Enter the value:");
+		int n = in.nextInt();
+		System.out.println(m*n);
+		
+		
+
+	}
+
+}
