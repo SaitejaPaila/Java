84f713450b4834bebcc924be076aa368d1faf198
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-03-01 00:41:04 -0600

    Add files via upload

---
+++ PailaSLab4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pailaslab4;
+
+/**
+ *
+ * @author saitejapaila
+ */
+public class PailaSLab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
