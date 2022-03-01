dd1833e0a6d7a3197572de6fa86740ab46b478c8
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-02-28 22:28:05 -0600

    Add files via upload

---
+++ PailaSLab1.java
@@ -0,0 +1,33 @@
+
+package pailaslab1;
+import java .util.*; 
+/**
+ * Date: January 17,2022
+ * @author saitejapaila
+ * Description : Lab 1
+ */
+public class PailaSLab1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("I can write Java now!");     //Task 3 
+        System.out.println();
+        System.out.println("Isn't that wonderful?");
+        System.out.println();
+        int x = 5;
+        System.out.println("The square of x is 25"); 
+           
+        System.out.println();                           //Task 4
+        Scanner input = new Scanner (System.in);        
+        System.out.println("Hello! What is your age?");
+        String age  = input.next();   //Taking input from the user
+        System.out.println("Cool " + age + " is a great age!");
+        System.out.println("Bubbye!");
+        
+      
+        
+        
+               
+    }
+    
+}
