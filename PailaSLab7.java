363fe26c5f48d1bfa950fc3f348fd62667c00532
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-03-01 00:42:33 -0600

    Add files via upload

---
+++ PailaSLab7.java
@@ -0,0 +1,34 @@
+
+package pailaslab7;
+import java.util.*;
+import java.io.*;
+
+
+public class PailaSLab7 {
+
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        int count = 0;
+        
+   
+        File file = new File("population by zip.txt");
+        Scanner fileInput = new Scanner(file);
+        PrintWriter output = new PrintWriter("dataSwitch.txt");
+        
+        while(fileInput.hasNextInt()){
+           int zipCode = fileInput.nextInt();
+           int population = fileInput.nextInt();
+           count ++;
+           
+           output.print(population + "   " + zipCode);
+           output.println();
+        }
+        
+        output.println();
+        output.println("The file had " + count + " lines");
+        fileInput.close();
+        output.close();
+        System.out.println("The file had " + count + " lines");
+    }
+    
+}
