530dac7be6594ae5af1ece4bf42ad0d0ca11d14d
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-02-28 23:57:55 -0600

    Create bubble sort.java

---
+++ bubble sort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+ 
+public class Bubblesort
+{
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+ 
+    public static void bubbleSort(int[] arr)
+    {
+        for (int k = 0; k < arr.length - 1; k++)
+        {
+           
+            for (int i = 0; i < arr.length - 1 - k; i++)
+            {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int[] arr = { 3, 5, 8, 4, 1, 9, -2 };
+ 
+        bubbleSort(arr);
+      
+      System.out.println(Arrays.toString(arr));
+    }
+}
