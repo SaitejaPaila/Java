e913a39f5c959680f75fad888b039e0a407c5192
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-02-28 22:26:10 -0600

    Add files via upload

---
+++ Excercise1Problem2.java
@@ -0,0 +1,21 @@
+
+package excercise1problem2;
+
+import java.util.Scanner;
+public class Excercise1Problem2 {
+
+    
+    public static void main(String[] args) {
+        int current_population = 312032486 ;
+        int one_year = 24 * 3600 * 365;
+        int new_birth = one_year/7;
+        int death = one_year/13;
+        int new_immigrant = one_year/45;
+        int new_population = new_birth + new_immigrant - death;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter the number of years: ");
+        int n = input.nextInt();
+        System.out.println("The population for " + n + " years is: " +(current_population + (new_population * n))) ;
+    }
+    
+}
