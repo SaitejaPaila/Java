a315e11c86dd5f17bcc16d4e5219856d79b56b5b
Saiteja <84107269+SaitejaPaila@users.noreply.github.com>
2022-03-01 00:39:27 -0600

    Update Userinput.java

--- Userinput.java
+++ Userinput.java
@@ -1,26 +1,22 @@
 
 package userinput;
-import java .util.*; 
-/**
- * Date: January 17,2022
- * @author saitejapaila
- * Description : Lab 1
- */
+import java.util.*; 
+
 public class Userinput {
 
     
     public static void main(String[] args) {
-        System.out.println("I can write Java now!");     //Task 3 
+        System.out.println("I can write Java now!");    
         System.out.println();
         System.out.println("Isn't that wonderful?");
         System.out.println();
         int x = 5;
         System.out.println("The square of x is 25"); 
            
-        System.out.println();                           //Task 4
+        System.out.println();                          
         Scanner input = new Scanner (System.in);        
         System.out.println("Hello! What is your age?");
-        String age  = input.next();   //Taking input from the user
+        String age  = input.next();   
         System.out.println("Cool " + age + " is a great age!");
         System.out.println("Bubbye!");
         
